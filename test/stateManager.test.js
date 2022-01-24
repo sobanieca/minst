451f81818d19001711a20d93a8f2056b83490126
Adam Sobaniec <sobanieca@gmail.com>
2022-01-24 22:18:09 +0100

    assertthrows test

--- test/stateManager.test.js
+++ test/stateManager.test.js
@@ -1,6 +1,7 @@
 import {
   assert,
   assertEquals,
+  assertThrows
 } from "https://deno.land/std@0.119.0/testing/asserts.ts";
 
 const defaultStateName = "test";
@@ -14,9 +15,9 @@ const getTestStateManager = async () => {
   return stateManagerModule.default;
 };
 
-const getState = async () => {
+const getState = async (stateName, writerName) => {
   const stateManager = await getTestStateManager();
-  const testState = stateManager.getState(defaultStateName, defaultWriterName);
+  const testState = stateManager.getState(stateName, writerName);
 
   const stateChangeData = {};
   const verifyStateChange = (p, n, w) => {
@@ -85,7 +86,11 @@ Deno.test("changing-nested-object-should-trigger-subscription", async () => {
   assertEquals(stateChangeData.writerName, defaultWriterName);
 });
 
-//Deno.test("changing-property-without-writername-should-fail");
+Deno.test("changing-property-without-writername-should-fail", async () => {
+  const { testState, stateChangeData } = await getState(defaultStateName);
+
+  assertThrows(() => testState.value = "value1", Error, "writerName");
+});
 //Deno.test("non-string-statename-provided-should-fail");
 //Deno.test("multiple-subscriptions-added-should-trigger-all");
 //Deno.test("non-string-statename-provided-should-fail");
